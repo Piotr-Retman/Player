fb2d28258bd87bcbc27007102494772f6f5bfba5
Piotr-Retman <obywatel23@gmail.com>
2016-01-31 20:47:37 +0100

    Change

---
+++ src/main/java/com/example/Player/MyActivity.java
@@ -0,0 +1,58 @@
+package com.example.Player;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.player);
+        Button button = (Button) findViewById(R.id.button);
+//        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.song);
+//        mediaPlayer.start();
+//        currentPosition = (TextView) findViewById(R.id.textView);
+//        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.string);
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+}
